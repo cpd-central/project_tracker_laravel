c881effc7ef72fbe60a99e5f27677b19d8260654
Jake Marsnik <marsnikj1@gmail.com>
2019-05-24 17:16:11 -0500

    removed testing echos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,12 +9,6 @@
     <div class="col-md-8">
       <div class="card">
         <div class="card-header">{{ __('Register') }}</div>
-        {{ env('DB_HOST') }}
-        {{ env('DB_PORT') }} 
-        {{ env('DB_DATABASE') }}
-        {{ env('DB_PASSWORD') }}
-        {{ env('DB_USERNAME') }}
-
         <div class="card-body">
           <form method="POST" action="{{ route('register') }}">
             @csrf
