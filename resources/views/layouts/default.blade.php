4737da3dd28d64d6b84d97b6115f4a0d6cf13a68
Jake Marsnik <marsnikj1@gmail.com>
2019-05-24 16:14:59 -0500

    fixed navbar thing

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,13 +2,6 @@
 <html>
   <head>
     @include('includes.navbar')
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   </head>
   <body>
     <div class="container">
