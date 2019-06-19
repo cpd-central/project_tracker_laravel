0c722e99dcae705e59902b2c5f4cbe1bb03d2603
Randy Clintsman <Randy@cegengineers.com>
2019-06-19 15:42:45 -0500

    Users can now only see projects their associated with. This includes project they've created, ceg author of, or are the manager of. This also implies to Won Project Index.

--- app/User.php
+++ app/User.php
@@ -21,7 +21,7 @@ class User extends Eloquent implements Authenticatable
 	 * @var array
 	 */
 	protected $fillable = [
-			'name', 'email', 'password',
+			'name', 'email', 'password', 'role'
 	];
 
 	/**
