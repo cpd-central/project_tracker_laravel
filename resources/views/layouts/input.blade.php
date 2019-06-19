4f4b4018ec481687a428c26304374f1f744adb64
Randy Clintsman <Randy@cegengineers.com>
2019-06-19 09:56:23 -0500

    javascript now adds total of all the fields to make sure they add up to 100%. It's color coded so if it's 100% it's green but anything all it's red. Works in both New and Edit project. Also added caption telling the user to input decimals despite it not being implemented yet. Will be doing that next update.

--- resources/views/layouts/input.blade.php
+++ resources/views/layouts/input.blade.php
@@ -122,6 +122,15 @@
                       $row = (int) (count($project['monthlypercent']) / 4);             //4 text boxes per row
                       $mod = count($project['monthlypercent']) % 4;
                       $i = 0;
+
+                      //caption header  ?>
+                       <tr>
+                        <td colspan="100%">
+                          <h5 class="text-center">Input percents as decimals below. For reference: 0.05 = 5%, 0.7 = 70%, 1 = 100%</h5>
+                        </td>
+                        </tr>
+
+                      <?php
                       while($i < count($project['monthlypercent'])) {
                         for($k = 1; $k <= $row; $k++) { ?>
                           <tr>
@@ -132,7 +141,7 @@
                             }?>
                           <td>{{$month[$mm].$year}}</td>
                                         <td>
-                                            <input type="number" class="form-control" id="month'+i+'year'+year+'" name="monthly_percent[]" value="{{$project['monthlypercent'][$i]}}" />
+                                            <input type="number" class="form-control" id="{{'month'.$mm.'year'.$year}}" name="monthly_percent[]" value="{{$project['monthlypercent'][$i]}}" />
                                         </td>
                                     <?php $mm++;
                                           $i++;
@@ -148,7 +157,7 @@
                             } ?>
                             <td>{{$month[$mm].$year}}</td>
                                         <td>
-                                            <input type="number" class="form-control" id="month'+i+'year'+year+'" name="monthly_percent[]" value="{{$project['monthlypercent'][$i]}}" />
+                                            <input type="number" class="form-control" id="{{'month'.$mm.'year'.$year}}" name="monthly_percent[]" value="{{$project['monthlypercent'][$i]}}" />
                                         </td>
                                      <?php $mm++;
                                            $i++; 
@@ -279,29 +288,38 @@
       </form>
     </div>
 
+
+
     <script type="text/javascript">
     var datentp;
     var dateenergization;
+    var numFields;
+    var start_month;
+    var start_year;
       $(document).ready(function() {
-        //check();
         $("#datentp").on('blur', function() {
           removeFields();
           datentp = format('#datentp');
           dateenergization = format('#dateenergization');
           check();
         });
+
         $("#dateenergization").on('blur', function() {
           removeFields();
           datentp = format('#datentp');
           dateenergization = format('#dateenergization');
           check();
         });
+
+        $("#dynamic_field").on('change', function() {
+          if(datentp != 0 && dateenergization != 0) {
+            addTotalBox();
+          }
+        });
       });
 
       function check() {
-        if(datentp != null && dateenergization != null) {
-          console.log("ntp"+datentp);
-          console.log("ener"+dateenergization);
+        if(datentp != 0 && dateenergization != 0) {
           datentp = new Date(datentp);                   
           dateenergization = new Date(dateenergization); 
           calculateFields(monthDiff(datentp, dateenergization));
@@ -327,18 +345,25 @@
       }
 
       function calculateFields($var){
-        console.log($var);
+        numFields = $var;
         var row = $var / 4;             //4 text boxes per row
         var mod = $var % 4;
 
-        var start_month = datentp.getMonth();
-        var start_year = datentp.getFullYear();
+        start_month = datentp.getMonth();
+        start_year = datentp.getFullYear();
 
         var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
         var i = start_month;
         var year = start_year;
-        
+
+        //Caption
+          var tr =  '<tr>' +
+                        '<td colspan="100%">' +
+                          '<h5 class="text-center">'+"Input percents as decimals below. For reference: 0.05 = 5%, 0.7 = 70%, 1 = 100%"+'</h5>'
+                        '</td>' +
+                  '</tr>';
+                  $('#dynamic_field').append(tr);
         for(var k = 1; k <= row; k++) {
           var tr = '<tr>';
           for (var j = 0; j < 4; j++) {
@@ -370,6 +395,49 @@
           }
           var tr = tr + '</tr>';          
           $('#dynamic_field').append(tr);
+        }
+        addTotalBox();
+      }
+
+      function addTotalBox() {
+        $("#total_box").remove();
+        if($('#datentp').val() && $('#dateenergization').val()){
+          datentp = new Date($('#datentp').val());
+          dateenergization = new Date($('#dateenergization').val());
+          numFields = monthDiff(datentp, dateenergization);
+        }
+        var i =  parseInt(datentp.getMonth());
+        var year = parseInt(datentp.getFullYear());
+        var total = 0;
+        for(j=0; j < numFields; j++){
+          if(i > 11){
+              i = 0;
+              year = year + 1;
+            }
+          var string = '#month'+i+'year'+year;
+          if(!isNaN(parseFloat($(string).val()))) {
+            total += parseFloat($(string).val());
+            i++;
+          }
+          else{
+            i++;
+          }
+        }
+        if(total == 100){
+          var tr = '<tr id="total_box">' +
+                        '<td>' +
+                          '<div class="p-3 mb-2 bg-success text-white">'+total+"%"+'</div>'
+                        '</td>' +
+                  '</tr>';
+            $('#dynamic_field').append(tr);   
+        }
+        else{
+          var tr = '<tr id="total_box">' +
+                        '<td>' +
+                          '<div class="p-3 mb-2 bg-danger text-white">'+total+"%"+'</div>'
+                        '</td>' +
+                  '</tr>';
+            $('#dynamic_field').append(tr);       
         }
       }
     </script>
