2c1d80b7cc6177839b6ff983109bd2e3b60c1d02
Randy Clintsman <Randy@cegengineers.com>
2019-07-19 15:42:36 -0500

    Now saves old data.

--- app/Http/Controllers/TimesheetController.php
+++ app/Http/Controllers/TimesheetController.php
@@ -35,6 +35,15 @@ class TimesheetController extends Controller
         return $php_date;
     }
 
+    protected function arrayFormat($new_array, $old_array, $daterangeArray){
+        foreach($daterangeArray as $date){
+            if((!isset($new_array[$date])) && isset($old_array[$date])){
+                unset($old_array[$date]);
+            }
+        }
+        return $old_array;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -65,73 +74,149 @@ class TimesheetController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store($timesheet, $request)
-    {   //dd($request);
-        $codes = array();
-        //Store code CEG
-        $daterangeArray = $request->get('daterange');
-        $CEG = array();
-        $CEG['General and Admin'] = $this->databasePrep($this->formatArray($request->get('row0'), $daterangeArray));
-        $CEG['Staff Meetings and HR'] = $this->databasePrep($this->formatArray($request->get('row1'), $daterangeArray));
-        $codes['CEG'] = $CEG;
-        //$timesheet->CEG = $CEG;
+    {   if($timesheet['Codes']){                //Enter this if there was a previous timesheet
+            //dd($request);
+            $codes = array();
+            //Store code CEG
+            $daterangeArray = $request->get('daterange');
+            $CEG = array();
+            $CEG['General and Admin'] = $this->databasePrep($this->formatArray($request->get('row0'), $daterangeArray));
+            $CEG['General and Admin'] += $this->arrayFormat($CEG['General and Admin'], $timesheet['Codes']['CEG']['General and Admin'], $daterangeArray);
 
-        //Store Code CEGTRNG
-        $CEGTRNG = array();
-        $CEGTRNG['Research and Training'] = $this->databasePrep($this->formatArray($request->get('row2'), $daterangeArray));
-        $codes['CEGTRNG'] = $CEGTRNG;
-        //$timesheet->CEGTRNG = $CEGTRNG;
+            $CEG['Staff Meetings and HR'] = $this->databasePrep($this->formatArray($request->get('row1'), $daterangeArray));
+            $CEG['Staff Meetings and HR'] += $this->arrayFormat($CEG['Staff Meetings and HR'], $timesheet['Codes']['CEG']['Staff Meetings and HR'], $daterangeArray);
+            $codes['CEG'] = $CEG;
 
-        //Store Code CEGEDU
-        $CEGEDU = array();
-        $CEGEDU['Formal EDU'] = $this->databasePrep($this->formatArray($request->get('row3'), $daterangeArray));
-        $codes['CEGEDU'] = $CEGEDU;
-        //$timesheet->CEGEDU = $CEGEDU;
+            //Store Code CEGTRNG
+            $CEGTRNG = array();
+            $CEGTRNG['Research and Training'] = $this->databasePrep($this->formatArray($request->get('row2'), $daterangeArray));
+            $CEGTRNG['Research and Training'] += $this->arrayFormat($CEGTRNG['Research and Training'], $timesheet['Codes']['CEGTRNG']['Research and Training'], $daterangeArray);
+            $codes['CEGTRNG'] = $CEGTRNG;
 
-        //Store Code CEGMKTG
-        $CEGMKTG = array();
-        $CEGMKTG['General Marketing'] = $this->databasePrep($this->formatArray($request->get('row4'), $daterangeArray));
-        $codes['CEGMKTG'] = $CEGMKTG;
-        //$timesheet->CEGMKTG = $CEGMKTG;
+            //Store Code CEGEDU
+            $CEGEDU = array();
+            $CEGEDU['Formal EDU'] = $this->databasePrep($this->formatArray($request->get('row3'), $daterangeArray));
+            $CEGEDU['Formal EDU'] += $this->arrayFormat($CEGEDU['Formal EDU'], $timesheet['Codes']['CEGEDU']['Formal EDU'], $daterangeArray);
+            $codes['CEGEDU'] = $CEGEDU;
 
-        //Added rows
-        $row = (int) $request->get('row_total');
-        if($row > 4) {
-            $arrayCodes = array();
-            $descriptions = array();
-            for($i = 5; $i <= $row; $i++){
-                if($request->get('row'.$i) != null){
-                    if(array_sum($request->get('row'.$i)) > 0){
-                        $arr = array();
-                        $string = $request->get('Product_Description_row_'.$i);
-                        $arr[$string] = $this->databasePrep($this->formatArray($request->get('row'.$i), $daterangeArray));
-                        $code = $request->get('codeadd'.$i);
+            //Store Code CEGMKTG
+            $CEGMKTG = array();
+            $CEGMKTG['General Marketing'] = $this->databasePrep($this->formatArray($request->get('row4'), $daterangeArray));
+            $CEGMKTG['General Marketing'] += $this->arrayFormat($CEGMKTG['General Marketing'], $timesheet['Codes']['CEGMKTG']['General Marketing'], $daterangeArray);
+            $codes['CEGMKTG'] = $CEGMKTG;
 
-                        if(array_key_exists($code, $arrayCodes)){
-                            $descriptions = $arrayCodes[$code];
-                            array_push($descriptions, $string);
-                            $arrayCodes[$code] = $descriptions;
-                        }
-                        else{
-                            $descriptions = array();
-                            array_push($descriptions, $string);
-                            $arrayCodes[$code] = $descriptions;
-                        }
-                        if(isset($codes[$code])){
-                            $codes[$code] = array_merge($codes[$code], $arr);   //might need to be fixed in future with edit
-                        }
-                        else{
-                            $codes[$code] = $arr;
+            //Added rows
+            $row = (int) $request->get('row_total');
+            if($row > 4) {
+                $arrayCodes = array();
+                $descriptions = array();
+                for($i = 5; $i <= $row; $i++){
+                    if($request->get('row'.$i) != null){
+                        if(array_sum($request->get('row'.$i)) > 0){
+                            $arr = array();
+                            $string = $request->get('Product_Description_row_'.$i);
+                            $code = $request->get('codeadd'.$i);
+                            $arr[$string] = $this->databasePrep($this->formatArray($request->get('row'.$i), $daterangeArray));
+                            if(isset($timesheet['Codes'][$code][$string])){
+                                $arr[$string] += $this->arrayFormat($arr[$string], $timesheet['Codes'][$code][$string], $daterangeArray);
+                            }
+                            if(array_key_exists($code, $arrayCodes)){
+                                $descriptions = $arrayCodes[$code];
+                                array_push($descriptions, $string);
+                                $arrayCodes[$code] = $descriptions;
+                            }
+                            else{
+                                $descriptions = array();
+                                array_push($descriptions, $string);
+                                $arrayCodes[$code] = $descriptions;
+                            }
+                            if(isset($codes[$code])){
+                                $codes[$code] = array_merge($codes[$code], $arr);   //might need to be fixed in future with edit
+                            }
+                            else{
+                                $codes[$code] = $arr;
+                            }
                         }
                     }
                 }
+                $keys_old = array_keys($timesheet['Codes']);
+                foreach($keys_old as $key){
+                    if(!isset($codes[$key]) && $key != "Additional_Codes"){
+                        $codes[$key] = $timesheet['Codes'][$key];
+                    }
+                }
+                if(count($arrayCodes) > 0){
+                    $Additional_Codes = $arrayCodes;
+                    $codes["Additional_Codes"] = $Additional_Codes;
+                }
             }
-            if(count($arrayCodes) > 0){
-                $Additional_Codes = $arrayCodes;
-                $codes["Additional_Codes"] = $Additional_Codes;
-            }
+            $timesheet->Codes = $codes;
+            $timesheet->save();
         }
-        $timesheet->Codes = $codes;
-        $timesheet->save();
+        else{                       //If there wasn't a previous timesheet, don't need to worry about overwriting.
+            $codes = array();
+            //Store code CEG
+            $daterangeArray = $request->get('daterange');
+            $CEG = array();
+            $CEG['General and Admin'] = $this->databasePrep($this->formatArray($request->get('row0'), $daterangeArray));
+            $CEG['Staff Meetings and HR'] = $this->databasePrep($this->formatArray($request->get('row1'), $daterangeArray));
+            $codes['CEG'] = $CEG;
+
+            //Store Code CEGTRNG
+            $CEGTRNG = array();
+            $CEGTRNG['Research and Training'] = $this->databasePrep($this->formatArray($request->get('row2'), $daterangeArray));
+            $codes['CEGTRNG'] = $CEGTRNG;
+
+            //Store Code CEGEDU
+            $CEGEDU = array();
+            $CEGEDU['Formal EDU'] = $this->databasePrep($this->formatArray($request->get('row3'), $daterangeArray));
+            $codes['CEGEDU'] = $CEGEDU;
+
+            //Store Code CEGMKTG
+            $CEGMKTG = array();
+            $CEGMKTG['General Marketing'] = $this->databasePrep($this->formatArray($request->get('row4'), $daterangeArray));
+            $codes['CEGMKTG'] = $CEGMKTG;
+
+            //Added rows
+            $row = (int) $request->get('row_total');
+            if($row > 4) {
+                $arrayCodes = array();
+                $descriptions = array();
+                for($i = 5; $i <= $row; $i++){
+                    if($request->get('row'.$i) != null){
+                        if(array_sum($request->get('row'.$i)) > 0){
+                            $arr = array();
+                            $string = $request->get('Product_Description_row_'.$i);
+                            $arr[$string] = $this->databasePrep($this->formatArray($request->get('row'.$i), $daterangeArray));
+                            $code = $request->get('codeadd'.$i);
+
+                            if(array_key_exists($code, $arrayCodes)){
+                                $descriptions = $arrayCodes[$code];
+                                array_push($descriptions, $string);
+                                $arrayCodes[$code] = $descriptions;
+                            }
+                            else{
+                                $descriptions = array();
+                                array_push($descriptions, $string);
+                                $arrayCodes[$code] = $descriptions;
+                            }
+                            if(isset($codes[$code])){
+                                $codes[$code] = array_merge($codes[$code], $arr);   //might need to be fixed in future with edit
+                            }
+                            else{
+                                $codes[$code] = $arr;
+                            }
+                        }
+                    }
+                }
+                if(count($arrayCodes) > 0){
+                    $Additional_Codes = $arrayCodes;
+                    $codes["Additional_Codes"] = $Additional_Codes;
+                }
+            }
+            $timesheet->Codes = $codes;
+            $timesheet->save();
+        }
     }
 
     public function check()
