b5dcb9fe19c1196e0e7683ffaf70d8d2e802fa46
Sam Young <samyoung@Sams-MacBook-Pro-815.local>
2020-05-28 15:38:01 -0500

    changed planner page

--- resources/views/pages/planner.blade.php
+++ resources/views/pages/planner.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.index')
 
+@section('table-title', 'Projects to Manage')
+
+@section('table-header')
+<table class="table table-striped">
+  <thead>
+    <tr> 
+      <th>Action</th>
+      <th>Project Name</th>
+    </tr>
+  </thead>
+@stop
+
 @section('table-content')
 @foreach($projects as $project)
     <tr>
-        <td><a href="{{action('ProjectController@manage_project', $project['_id'])}}"</a></td>
+        <td><a href="{{action('ProjectController@manage_project', $project['_id'])}}" class="btn btn-warning">Manage Project</a></td>
         <td>{{$project['projectname']}}</td>
     </tr>
 @endforeach
+@stop
