12913ddedadd2a6cdbfdaa67febaa6caf9c6a759
Randall Clintsman <rookid9492@hotmail.com>
2020-05-28 15:13:42 -0500

    Removed old tracker files

--- resources/views/pages/edittracker.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.trackerinput')
-
-@section('page-title', 'New Tracker Project')
-@section('title', 'New Tracker Project')
-@section('project-name', $project['name'])
-@section('project-due', $project['due'])
--- resources/views/pages/newtrackerproject.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.trackerinput')
-
-@section('page-title', 'New Tracker Project')
-@section('title', 'New Tracker Project')
-@section('project-name', '')
-@section('project-due', '')
