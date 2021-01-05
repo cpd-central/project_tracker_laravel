c6232ffa8f97afc141924e07bfb76045c6a5f9b2
Randy Clintsman <rclintsman@ceg-engineers.com>
2021-01-05 15:43:46 -0600

    got data pulled, removes employees without hours, totals the hours.

--- resources/views/pages/hourstable.blade.php
+++ resources/views/pages/hourstable.blade.php
@@ -13,13 +13,58 @@
 <?php 
 if(isset($code)){
     echo $code;
-    if(isset($project)){
+    if(isset($project[0])){
         $hours_data = $project[0]['hours_data'];
         $year = array_keys($hours_data);
         print " {$year[0]}";
-        $months_array = array_keys($year[0]);
-        
-    }
-}
+        $months_array = array_keys($hours_data[$year[0]]);
+        $employee_array = array_keys($hours_data[$year[0]][$months_array[0]]);
+        unset($employee_array[count($employee_array) - 1]); //removes "Total" employee
+        $total_employee = [count($employee_array)];
+        for($j = 0; $j < count($employee_array); $j++){ 
+            $total_employee[$j] = 0;
+        }
+        for($i = 0; $i < count($months_array); $i++){
+            for($j = 0; $j < count($employee_array); $j++){ 
+            $total_employee[$j] += $hours_data[$year[0]][$months_array[$i]][$employee_array[$j]];
+            }
+        }
+        /////////////////////////////////////////////////////unset offsets the $j
+        $count_employees = count($employee_array);
+        for($j = 0; $j < $count_employees; $j++){ 
+            if($total_employee[$j] == 0){
+                unset($employee_array[$j]);
+                unset($total_employee[$j]);
+            }
+        }
 ?>
+
+<table class="table table-striped">
+<th>
+    <?php foreach($employee_array as $j){ ?>
+    <td>{{$j}}</td>
+    <?php }?>
+</th>
+<?php for($i = 0; $i < count($months_array); $i++){ ?>
+<tr>
+    <td>{{$months_array[$i]}}</td>
+    <?php foreach($employee_array as $j){ ?>
+    <td>{{$hours_data[$year[0]][$months_array[$i]][$j]}}</td>
+    <?php }?>
+</tr>
+
+<?php } ?>
+<tr>
+    <td>Total</td>
+    <?php foreach($total_employee as $j){ ?>
+    <td>{{$j}}</td>
+    <?php }?>
+</tr>
+<?php    }
+
+    else{
+        print " has no project associated.";
+    } 
+} ?>
+</table>
 @endsection
